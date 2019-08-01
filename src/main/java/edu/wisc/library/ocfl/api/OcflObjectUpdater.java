0d71d69a68b8b815e2b4b1b6463cb402fdbd2f2a
Peter Winckles <peter.winckles@wisc.edu>
2019-08-01 14:52:15 -0500

    added support for selectively modifying existing objects

---
+++ src/main/java/edu/wisc/library/ocfl/api/OcflObjectUpdater.java
@@ -0,0 +1,37 @@
+package edu.wisc.library.ocfl.api;
+
+import java.nio.file.Path;
+
+public interface OcflObjectUpdater {
+
+    // TODO Path vs String?
+    // TODO should there be errors if destinationPaths already exist? perhaps a force flag?
+
+    /**
+     * Adds a file or directory to the object being operated on. The destinationPath is where the file should be inserted
+     * into the object relative to the object's root.
+     *
+     * @param sourcePath
+     * @param destinationPath
+     * @return
+     */
+    OcflObjectUpdater addPath(Path sourcePath, String destinationPath);
+
+    /**
+     * Removes a file from an object. The given path should be relative to the object's root.
+     *
+     * @param path
+     * @return
+     */
+    OcflObjectUpdater removeFile(String path);
+
+    /**
+     * Renames an existing file within an object. Both paths are relative the object's root.
+     *
+     * @param sourcePath
+     * @param destinationPath
+     * @return
+     */
+    OcflObjectUpdater renameFile(String sourcePath, String destinationPath);
+
+}
--- src/main/java/edu/wisc/library/ocfl/api/OcflRepository.java
+++ src/main/java/edu/wisc/library/ocfl/api/OcflRepository.java
@@ -1,14 +1,42 @@
 package edu.wisc.library.ocfl.api;
 
 import java.nio.file.Path;
+import java.util.function.Consumer;
 
 public interface OcflRepository {
 
-    // TODO support specifying multiple paths? how to handle the merge?
+    /**
+     * Adds the object rooted at the given path to the OCFL repository under the given objectId. If their is an existing
+     * object with the id, then a new version of the object is created.
+     *
+     * It is important to note that the files present in the given path should comprise the entirety of the object. Files
+     * from previous versions that are no longer present in the path are not carried over. At the same time, files that
+     * are unchanged between object versions are not stored a second time.
+     *
+     * This method should only be used when writing new or fully composed objects.
+     *
+     * @param objectId
+     * @param path
+     * @param commitMessage
+     */
+    // TODO this should probably return a reference to the new version that was created
     void putObject(String objectId, Path path, CommitMessage commitMessage);
+
+    /**
+     * Updates an existing object by selectively adding and removing files and creating a new version that encapsulates
+     * all of the changes.
+     *
+     * @param objectId
+     * @param commitMessage
+     */
+    // TODO this should probably return a reference to the new version that was created
+    void updateObject(String objectId, CommitMessage commitMessage, Consumer<OcflObjectUpdater> objectUpdater);
 
     void getObject(String objectId, Path outputPath);
 
     void getObject(String objectId, String versionId, Path outputPath);
+
+    // TODO perhaps there should be a getObject method that operates on a Consumer<>, providing a virtual view of an object
+    //      this would be read-only. a read-write version would also be possible, but creates a bigger locking problem.
 
 }
