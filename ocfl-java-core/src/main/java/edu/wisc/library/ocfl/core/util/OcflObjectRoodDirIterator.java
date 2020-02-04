516d8b39447029d3043641225c475b5764951e93
Peter Winckles <peter.winckles@wisc.edu>
2020-02-04 07:57:56 -0600

    added license header to all source files

--- ocfl-java-core/src/main/java/edu/wisc/library/ocfl/core/util/OcflObjectRoodDirIterator.java
+++ ocfl-java-core/src/main/java/edu/wisc/library/ocfl/core/util/OcflObjectRoodDirIterator.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 University of Wisconsin Board of Regents
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package edu.wisc.library.ocfl.core.util;
 
 import edu.wisc.library.ocfl.api.util.Enforce;
@@ -17,7 +41,6 @@ import java.util.NoSuchElementException;
  * Iterator that iterates over OCFL object root directories. Object roots are identified by the presence of a file that's
  * prefixed with '0=ocfl_object'.
  */
-// TODO needs more tests!
 class OcflObjectRoodDirIterator implements Iterator<Path>, Closeable {
 
     private static final String OCFL_OBJECT_MARKER_PREFIX = "0=ocfl_object";
@@ -69,12 +92,12 @@ class OcflObjectRoodDirIterator implements Iterator<Path>, Closeable {
                 var nextDirectory = fetchNextDirectory();
 
                 while (nextDirectory != null) {
-                    var children = Files.newDirectoryStream(nextDirectory, p -> {
+                    var objectMarkers = Files.newDirectoryStream(nextDirectory, p -> {
                         return p.getFileName().toString().startsWith(OCFL_OBJECT_MARKER_PREFIX);
                     });
 
                     // Found OCFL object marker -- current directory is an OCFL object root
-                    if (children.iterator().hasNext()) {
+                    if (objectMarkers.iterator().hasNext()) {
                         // Do not process children
                         popDirectory();
                         next = nextDirectory;
