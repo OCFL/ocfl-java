aef97a0a54e0c494c201ee5ce43546b04bfbbb02
Peter Winckles <peter.winckles@wisc.edu>
2020-02-14 14:15:39 -0600

    tweak travis windows setup

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,5 +3,5 @@
 if [ $TRAVIS_OS_NAME = 'windows' ]; then
   export JAVA_HOME=${JAVA_HOME:-/c/jdk}
   export PATH=${JAVA_HOME}/bin:${PATH}
-  choco install openjdk11 -params 'installdir=c:\\jdk' -y
+  choco install openjdk11 -y
 fi
