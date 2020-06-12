e6f8daf8f5b2bac2301909a755ee28eaae926279
Peter Winckles <peter.winckles@wisc.edu>
2020-06-12 13:10:18 -0500

    removed modularization due to issues related to one of the modules not being able to be modularized

--- ocfl-java-test/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module edu.wisc.library.ocfl.test {
-
-    exports edu.wisc.library.ocfl.test.matcher;
-
-    requires edu.wisc.library.ocfl.api;
-    requires org.hamcrest;
-    requires org.junit.jupiter.api;
-
-}
