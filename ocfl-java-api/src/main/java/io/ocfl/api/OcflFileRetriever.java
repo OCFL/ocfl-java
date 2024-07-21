ef82589fa0a1b8076c6f32f0ebe13be2628f1d49
Peter Winckles <pwinckles@pm.me>
2024-07-21 18:34:22 -0500

    add support for range requests

--- ocfl-java-api/src/main/java/io/ocfl/api/OcflFileRetriever.java
+++ ocfl-java-api/src/main/java/io/ocfl/api/OcflFileRetriever.java
@@ -25,6 +25,7 @@
 package io.ocfl.api;
 
 import io.ocfl.api.io.FixityCheckInputStream;
+import java.io.InputStream;
 
 /**
  * This class is used to lazy-load object files. A new instance should be created for each file that's intended to be load.
@@ -41,4 +42,17 @@ public interface OcflFileRetriever {
      * @return FixityCheckInputStream of the file's content
      */
     FixityCheckInputStream retrieveFile();
+
+    /**
+     * Returns an input stream of the file's content between the specified byte range. startPosition and endPosition
+     * may be null, depending on the underlying implementation, and the meaning of a null value is also implementation
+     * dependent.
+     *
+     * <p>The caller is responsible for closing the stream. The input stream is buffered.
+     *
+     * @param startPosition the byte offset in the file to start reading, inclusive
+     * @param endPosition the byte offset in the file to stop reading, inclusive
+     * @return a buffered input stream containing the specified file data
+     */
+    InputStream retrieveRange(Long startPosition, Long endPosition);
 }
